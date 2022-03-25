da6617ffbb56502b90e1909fdf1d2452f85aa289
Talley Ouro <talleyouro@outlook.com>
2022-03-24 21:09:07 -0400

    added pointers

--- main.go
+++ main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
+	pointers()
+}
+
+func pointers() {
+	var firstname *string = new(string)
+	*firstname = "Abibou"
+
+	fmt.Println(firstname)
+}
+func start() {
 	fmt.Println("Hello World")
+	var myint int = 34
+	fmt.Println(myint)
+	var myfloat float32 = 12.4
+	fmt.Println(myfloat)
+
+	name := "Abibou"
+	fmt.Println(name)
+	number := 100
+	fmt.Println(number)
+
+	cp := complex(3, 4)
+	fmt.Println(cp)
+
+	a, b := 3, 4
+	fmt.Println(a, b)
 }
